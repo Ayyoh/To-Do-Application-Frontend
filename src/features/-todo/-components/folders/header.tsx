eef1d3c6667499434f2cd6fc2669e2bc5fcd9d0d
Tidus <suna@gmail.com>
2025-11-04 18:19:06 +0800

    fix: added 3 more skeletons

--- src/features/-todo/-components/folders/header.tsx
+++ src/features/-todo/-components/folders/header.tsx
@@ -58,7 +58,7 @@ function Header() {
         </header>
 
         <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             {isLoading ? (
               <SkeletonLoader />
             ) : Array.isArray(todos) && todos.length > 0 ? (
